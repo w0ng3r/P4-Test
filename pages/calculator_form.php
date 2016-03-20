43ee70060d2b9f9f249f57a4257a620750e45048
g-grace <g-grace@users.noreply.github.com>
2016-03-20 09:11:19 -0700

    Update calculator_form.php
    comments

--- pages/calculator_form.php
+++ pages/calculator_form.php
@@ -1,5 +1,20 @@
 <?php
 //calculator_form.php
+ @package unitTesting
+ @authorA: George Wong: <w0ng3r@gmail.com>
+ @authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+ @version 1.0 2015/04/30 
+ @link http://w0ng3r.dlinkddns.com/P4/run_all.php
+ @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
+ @see function_test.php
+ @see git_ItemClass.php
+ @see git_ItemFunctions.php
+ @see git_ItemOrderPage.php
+ @see order-test.php
+ @see run_all.php
+ @see suite.php
+ @see test-ItemClass.php
+ @todo none 
 
 if(isset($_POST['num1']))
 {
