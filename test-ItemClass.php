ccc83b2826d9bb677e951d1353d41c2b2bf8ed39
g-grace <g-grace@users.noreply.github.com>
2016-03-19 17:16:55 -0700

    Update test-ItemClass.php
    comments

--- test-ItemClass.php
+++ test-ItemClass.php
@@ -1,12 +1,27 @@
 <?php
 //test-ItemClass.php
-
+@package RSS-FEEDS
+@authorA: George Wong: <w0ng3r@gmail.com>
+@authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+@version 1.0 2015/04/30 
+@link http://w0ng3r.dlinkddns.com/P4/run_all.php
+@license http://www.apache.org/licenses/LICENSE-2.0
+@see function-test.php
+@see SQL.sql
+@see git_ItemClass.php
+@see git_ItemFunctions.php
+@see git_ItemOrderPage.php
+@see order-test.php
+@see run_all.php
+@see suite.php
+@see test-ItemClass.php
+@todo none 
 require_once 'simpletest/autorun.php';
 
 include 'git_ItemClass.php';
 
 class TestItemClass extends UnitTestCase {
-
+    //this is a function to test a new item adding into the list
     function TestConstructor()
     {
         $tempItem = new Item("Fish", "50", "Fresh Alaskan Cod! Just came in this morning!");
